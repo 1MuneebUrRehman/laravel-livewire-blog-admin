d0890aa4c92c0bf742076fa69585ef9bb8a4710d
muneebuforia <muneeb@uforiainfotech.com>
2025-11-18 11:29:14 +0500

    issue fixed

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -11,14 +10,46 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            ['name' => 'Technology', 'description' => 'Articles about technology and innovation'],
-            ['name' => 'Programming', 'description' => 'Programming tutorials and best practices'],
-            ['name' => 'Web Development', 'description' => 'Frontend and backend web development'],
-            ['name' => 'Mobile Development', 'description' => 'iOS and Android app development'],
-            ['name' => 'DevOps', 'description' => 'DevOps practices and tools'],
-            ['name' => 'Data Science', 'description' => 'Data analysis and machine learning'],
-            ['name' => 'Design', 'description' => 'UI/UX design and principles'],
-            ['name' => 'Business', 'description' => 'Business and entrepreneurship'],
+            [
+                'name'        => 'Technology',
+                'slug'        => 'technology',
+                'description' => 'Articles about technology and innovation'
+            ],
+            [
+                'name'        => 'Programming',
+                'slug'        => 'programming',
+                'description' => 'Programming tutorials and best practices'
+            ],
+            [
+                'name'        => 'Web Development',
+                'slug'        => 'web-development',
+                'description' => 'Frontend and backend web development'
+            ],
+            [
+                'name'        => 'Mobile Development',
+                'slug'        => 'mobile-development',
+                'description' => 'iOS and Android app development'
+            ],
+            [
+                'name'        => 'DevOps',
+                'slug'        => 'devops',
+                'description' => 'DevOps practices and tools'
+            ],
+            [
+                'name'        => 'Data Science',
+                'slug'        => 'data-science',
+                'description' => 'Data analysis and machine learning'
+            ],
+            [
+                'name'        => 'Design',
+                'slug'        => 'design',
+                'description' => 'UI/UX design and principles'
+            ],
+            [
+                'name'        => 'Business',
+                'slug'        => 'business',
+                'description' => 'Business and entrepreneurship'
+            ],
         ];
 
         foreach ($categories as $category) {
